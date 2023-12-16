d7cd2eb48baec6afc63ab8eb9c9fe64ec981f0d8
clakinger <clakinger@gmail.com>
2023-12-16 11:29:33 -0600

    fix site??

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,17 +255,7 @@
         // Poptrox.
         $main.poptrox({
             baseZIndex: 20000,
-            caption: function ($a) {
-                var $image_img = $a.children('img');
-                var data = exifDatas[$image_img.data('name')];
-                if (data === undefined) {
-                    // EXIF data					
-                    EXIF.getData($image_img[0], function () {
-                        data = exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
-                    });
-                }
-                return data !== undefined ? '<p>' + data + '</p>' : ' ';
-            },
+            caption: 'test',
             fadeSpeed: 300,
             onPopupClose: function () {
                 $body.removeClass('modal-active');
